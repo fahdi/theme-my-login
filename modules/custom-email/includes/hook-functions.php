3132dbbd66ca8e399bcce3de32371cc1f531ae48
jfarthing84 <jfarthing84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-16 16:38:37 +0000

    Clean up and add new actions/filters.
    git-svn-id: https://plugins.svn.wordpress.org/theme-my-login/trunk@206535 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/custom-email/includes/hook-functions.php
+++ modules/custom-email/includes/hook-functions.php
@@ -8,22 +8,22 @@ function jkf_tml_custom_email_headers() {
 
 function jkf_tml_custom_email_from($from_email) {
     global $theme_my_login;
-    return empty($theme_my_login->options['mail_from']) ? $from_email : $theme_my_login->options['mail_from'];
+    return empty($theme_my_login->options['email']['mail_from']) ? $from_email : $theme_my_login->options['email']['mail_from'];
 }
     
 function jkf_tml_custom_email_from_name($from_name) {
     global $theme_my_login;
-    return empty($theme_my_login->options['mail_from_name']) ? $from_name : $theme_my_login->options['mail_from_name'];
+    return empty($theme_my_login->options['email']['mail_from_name']) ? $from_name : $theme_my_login->options['email']['mail_from_name'];
 }
 
 function jkf_tml_custom_email_content_type($content_type) {
     global $theme_my_login;
-    return empty($theme_my_login->options['mail_content_type']) ? $content_type : 'text/' . $theme_my_login->options['mail_content_type'];
+    return empty($theme_my_login->options['email']['mail_content_type']) ? $content_type : 'text/' . $theme_my_login->options['email']['mail_content_type'];
 }
 
 function jkf_tml_custom_email_retrieve_pass_title($title, $user_id) {
 	global $theme_my_login;
-	return empty($theme_my_login->options['retrieve_pass_title']) ? $title : jkf_tml_custom_email_replace_vars($theme_my_login->options['retrieve_pass_title'], $user_id);
+	return empty($theme_my_login->options['email']['retrieve_pass']['title']) ? $title : jkf_tml_custom_email_replace_vars($theme_my_login->options['email']['retrieve_pass']['title'], $user_id);
 }
 
 function jkf_tml_custom_email_retrieve_pass_message($message, $key, $user_id) {
@@ -33,12 +33,12 @@ function jkf_tml_custom_email_retrieve_pass_message($message, $key, $user_id) {
 		'%loginurl%' => site_url('wp-login.php', 'login'),
 		'%reseturl%' => site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode($user->user_login), 'login')
 		);
-	return empty($theme_my_login->options['retrieve_pass_message']) ? $message : jkf_tml_custom_email_replace_vars($theme_my_login->options['retrieve_pass_message'], $user_id, $replacements);
+	return empty($theme_my_login->options['email']['retrieve_pass']['message']) ? $message : jkf_tml_custom_email_replace_vars($theme_my_login->options['email']['retrieve_pass']['message'], $user_id, $replacements);
 }
 
 function jkf_tml_custom_email_reset_pass_title($title, $user_id) {
 	global $theme_my_login;
-	return empty($theme_my_login->options['reset_pass_title']) ? $title : jkf_tml_custom_email_replace_vars($theme_my_login->options['reset_pass_title'], $user_id);
+	return empty($theme_my_login->options['email']['reset_pass']['title']) ? $title : jkf_tml_custom_email_replace_vars($theme_my_login->options['email']['reset_pass']['title'], $user_id);
 }
 
 function jkf_tml_custom_email_reset_pass_message($message, $new_pass, $user_id) {
@@ -47,12 +47,17 @@ function jkf_tml_custom_email_reset_pass_message($message, $new_pass, $user_id) 
 		'%loginurl%' => site_url('wp-login.php', 'login'),
 		'%user_pass%' => $new_pass
 		);	
-	return empty($theme_my_login->options['reset_pass_message']) ? $message : jkf_tml_custom_email_replace_vars($theme_my_login->options['reset_pass_message'], $user_id, $replacements);
+	return empty($theme_my_login->options['email']['reset_pass']['message']) ? $message : jkf_tml_custom_email_replace_vars($theme_my_login->options['email']['reset_pass']['message'], $user_id, $replacements);
+}
+
+function jkf_tml_custom_email_reset_pass_disable($enable) {
+	global $theme_my_login;
+	return ( $theme_my_login->options['email']['reset_pass']['admin_disable'] ) ? 0 : 1;
 }
 
 function jkf_tml_custom_email_new_user_title($title, $user_id) {
 	global $theme_my_login;
-	return empty($theme_my_login->options['new_user_notification_title']) ? $title : jkf_tml_custom_email_replace_vars($theme_my_login->options['new_user_notification_title'], $user_id);
+	return empty($theme_my_login->options['email']['new_user']['title']) ? $title : jkf_tml_custom_email_replace_vars($theme_my_login->options['email']['new_user']['title'], $user_id);
 }
 
 function jkf_tml_custom_email_new_user_message($message, $new_pass, $user_id) {
@@ -61,7 +66,12 @@ function jkf_tml_custom_email_new_user_message($message, $new_pass, $user_id) {
 		'%loginurl%' => site_url('wp-login.php', 'login'),
 		'%user_pass%' => $new_pass
 		);	
-	return empty($theme_my_login->options['new_user_notification_message']) ? $message : jkf_tml_custom_email_replace_vars($theme_my_login->options['new_user_notification_message'], $user_id, $replacements);
+	return empty($theme_my_login->options['email']['new_user']['message']) ? $message : jkf_tml_custom_email_replace_vars($theme_my_login->options['email']['new_user']['message'], $user_id, $replacements);
+}
+
+function jkf_tml_custom_email_new_user_admin_disable($enable) {
+	global $theme_my_login;
+	return ( $theme_my_login->options['email']['new_user']['admin_disable'] ) ? 0 : 1;
 }
 
 function jkf_tml_custom_email_replace_vars($text, $user_id = '', $replacements = array()) {
