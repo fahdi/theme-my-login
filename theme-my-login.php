bc58dd4bb749b49099d509bce8f1a6c24516e1c8
jfarthing84 <jfarthing84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-01 04:44:01 +0000

    I think we've earned a beta badge!
    git-svn-id: https://plugins.svn.wordpress.org/theme-my-login/trunk@211778 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- theme-my-login.php
+++ theme-my-login.php
@@ -3,7 +3,7 @@
 Plugin Name: Theme My Login
 Plugin URI: http://www.jfarthing.com/wordpress-plugins/theme-my-login-plugin
 Description: Themes the WordPress login, registration and forgot password pages according to your theme.
-Version: 5.0-alpha
+Version: 5.0-beta
 Author: Jeff Farthing
 Author URI: http://www.jfarthing.com
 Text Domain: theme-my-login
