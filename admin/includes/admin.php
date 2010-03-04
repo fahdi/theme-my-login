98187962c68f49325a910830a52fb7f02b80c3b7
jfarthing84 <jfarthing84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-04 06:09:08 +0000

    Add a flag to TML module settings form to check for newly activated/deactivated modules.
    git-svn-id: https://plugins.svn.wordpress.org/theme-my-login/trunk@212791 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/includes/admin.php
+++ admin/includes/admin.php
@@ -117,27 +117,28 @@ function jkf_tml_save_settings($settings) {
 	$settings['enable_css'] = isset($settings['enable_css']) ? 1 : 0;
 	$settings['enable_template_tag'] = isset($settings['enable_template_tag']) ? 1 : 0;
 	$settings['enable_widget'] = isset($settings['enable_widget']) ? 1 : 0;
-	//$settings['active_modules'] = isset($settings['active_modules']) ? (array) $settings['active_modules'] : array();
-	$settings['modules'] = isset($settings['modules']) ? (array) $settings['modules'] : array();
-	
-	// Set modules to be activated
-	if ( $activate = array_diff($settings['modules'], (array) $current['active_modules']) )
-		$settings['activate_modules'] = $activate;
+	if ( isset($_POST['tml_editing_modules']) ) {
+		$settings['modules'] = isset($settings['modules']) ? (array) $settings['modules'] : array();
 		
-	// Set modules to be deactivated
-	if ( $deactivate = array_diff((array) $current['active_modules'], $settings['modules']) )
-		$settings['deactivate_modules'] = $deactivate;
-		
-	// Unset 'modules' as it is only relevent here
-	unset($settings['modules']);
+		// Set modules to be activated
+		if ( $activate = array_diff($settings['modules'], (array) $current['active_modules']) )
+			$settings['activate_modules'] = $activate;
+			
+		// Set modules to be deactivated
+		if ( $deactivate = array_diff((array) $current['active_modules'], $settings['modules']) )
+			$settings['deactivate_modules'] = $deactivate;
+			
+		// Unset 'modules' as it is only relevent here
+		unset($settings['modules']);
+	}
 
 	// Merge current settings
     $settings = wp_parse_args($settings, $current);
 	
 	// Allow plugins/modules to add/modify settings
     $settings = apply_filters('tml_save_settings', $settings);
-        
-    return $settings;
+	
+	return $settings;
 }
 
 function jkf_tml_install() {
