c5761241698ecae0ee7cf7be0eb65e945306382a
jfarthing84 <jfarthing84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-07 21:51:01 +0000

    Fix title and links in themed profiles module.
    git-svn-id: https://plugins.svn.wordpress.org/theme-my-login/trunk@214719 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/themed-profiles/includes/hook-functions.php
+++ modules/themed-profiles/includes/hook-functions.php
@@ -4,9 +4,9 @@ function wdbj_tml_themed_profiles_site_url($url, $path, $orig_scheme = '') {
 	global $wp_rewrite, $current_user;
 	
 	if ( is_user_logged_in() ) {
-		if ( strpos('profile.php', $url) !== false ) {
+		if ( strpos($url, 'profile.php') !== false ) {
 			$orig_url = $url;
-			$url = get_permalink(wdbj_tml_get_option('page_id'));
+			$url = add_query_arg('action', 'profile', get_permalink(wdbj_tml_get_option('page_id')));
 			if ( strpos($orig_url, '?') ) {
 				$query = substr($orig_url, strpos($orig_url, '?') + 1);
 				parse_str($query, $r);
@@ -18,8 +18,8 @@ function wdbj_tml_themed_profiles_site_url($url, $path, $orig_scheme = '') {
 }
 
 function wdbj_tml_themed_profiles_title($title, $action) {
-	if ( 'profile' == $action || is_user_logged_in() )
-		$title = 'Your Profile';
+	if ( 'profile' == $action && is_user_logged_in() && '' == wdbj_tml_get_var('current_instance', 'instance_id') )
+		$title = __('Your Profile', 'theme-my-login');
 	return $title;
 }
 
