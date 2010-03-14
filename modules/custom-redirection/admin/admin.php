ed17642688e550a78f0f0bd9a07da0dd6de3e6e9
jfarthing84 <jfarthing84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-14 23:02:04 +0000

    Add inline setting descriptions to the "Custom Redirection" module.
    git-svn-id: https://plugins.svn.wordpress.org/theme-my-login/trunk@217623 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/custom-redirection/admin/admin.php
+++ modules/custom-redirection/admin/admin.php
@@ -19,19 +19,25 @@ function wdbj_tml_custom_redirect_user_role_admin_page($role) {
     <tr valign="top">
 		<th scope="row"><?php _e('Log in', 'theme-my-login'); ?></th>
         <td>
-			<input name="theme_my_login[redirection][<?php echo $role; ?>][login_type]" type="radio" id="theme_my_login_redirection_<?php echo $role; ?>_login_type_default" value="default"<?php checked('default', $redirection['login_type']); ?> /> <label for="theme_my_login_redirection_<?php echo $role; ?>_login_type_default"><?php _e('Default', 'theme-my-login'); ?></label><br />
-            <input name="theme_my_login[redirection][<?php echo $role; ?>][login_type]" type="radio" id="theme_my_login_redirection_<?php echo $role; ?>_login_type_referer" value="referer"<?php checked('referer', $redirection['login_type']); ?> /> <label for="theme_my_login_redirection_<?php echo $role; ?>_login_type_referer"><?php _e('Referer', 'theme-my-login'); ?></label><br />
+			<input name="theme_my_login[redirection][<?php echo $role; ?>][login_type]" type="radio" id="theme_my_login_redirection_<?php echo $role; ?>_login_type_default" value="default"<?php checked('default', $redirection['login_type']); ?> /> <label for="theme_my_login_redirection_<?php echo $role; ?>_login_type_default"><?php _e('Default', 'theme-my-login'); ?></label>
+			<p class="description"><?php _e('Check this option to send the user to their WordPress Dashboard/Profile.', 'theme-my-login'); ?></p>
+            <input name="theme_my_login[redirection][<?php echo $role; ?>][login_type]" type="radio" id="theme_my_login_redirection_<?php echo $role; ?>_login_type_referer" value="referer"<?php checked('referer', $redirection['login_type']); ?> /> <label for="theme_my_login_redirection_<?php echo $role; ?>_login_type_referer"><?php _e('Referer', 'theme-my-login'); ?></label>
+			<p class="description"><?php _e('Check this option to send the user back to the page they were visiting before logging in.', 'theme-my-login'); ?></p>
 			<input name="theme_my_login[redirection][<?php echo $role; ?>][login_type]" type="radio" id="theme_my_login_redirection_<?php echo $role; ?>_login_type_custom" value="custom"<?php checked('custom', $redirection['login_type']); ?> />
 			<input name="theme_my_login[redirection][<?php echo $role; ?>][login_url]" type="text" id="theme_my_login_redirection_<?php echo $role; ?>_login_url" value="<?php echo $redirection['login_url']; ?>" class="regular-text" />
+			<p class="description"><?php _e('Check this option to send the user to a custom location, specified by the textbox above.', 'theme-my-login'); ?></p>
         </td>
     </tr>
     <tr valign="top">
 		<th scope="row"><?php _e('Log out', 'theme-my-login'); ?></th>
         <td>
 			<input name="theme_my_login[redirection][<?php echo $role; ?>][logout_type]" type="radio" id="theme_my_login_redirection_<?php echo $role; ?>_logout_type_default" value="default"<?php checked('default', $redirection['logout_type']); ?> /> <label for="theme_my_login_redirection_<?php echo $role; ?>_logout_type_default"><?php _e('Default', 'theme-my-login'); ?></label><br />
-            <input name="theme_my_login[redirection][<?php echo $role; ?>][logout_type]" type="radio" id="theme_my_login_redirection_<?php echo $role; ?>_logout_type_referer" value="referer"<?php checked('referer', $redirection['logout_type']); ?> /> <label for="theme_my_login_redirection_<?php echo $role; ?>_logout_type_referer"><?php _e('Referer', 'theme-my-login'); ?></label><br />
+            <p class="description"><?php _e('Check this option to send the user to the log in page, displaying a message that they have successfully logged out.', 'theme-my-login'); ?></p>
+			<input name="theme_my_login[redirection][<?php echo $role; ?>][logout_type]" type="radio" id="theme_my_login_redirection_<?php echo $role; ?>_logout_type_referer" value="referer"<?php checked('referer', $redirection['logout_type']); ?> /> <label for="theme_my_login_redirection_<?php echo $role; ?>_logout_type_referer"><?php _e('Referer', 'theme-my-login'); ?></label><br />
+			<p class="description"><?php _e('Check this option to send the user back to the page they were visiting before logging out. (Note: If the previous page being visited was an admin page, this can have unexpected results.)', 'theme-my-login'); ?></p>
 			<input name="theme_my_login[redirection][<?php echo $role; ?>][logout_type]" type="radio" id="theme_my_login_redirection_<?php echo $role; ?>_logout_type_custom" value="custom"<?php checked('custom', $redirection['logout_type']); ?> />
 			<input name="theme_my_login[redirection][<?php echo $role; ?>][logout_url]" type="text" id="theme_my_login_redirection_<?php echo $role; ?>_logout_url" value="<?php echo $redirection['logout_url']; ?>" class="regular-text" />
+			<p class="description"><?php _e('Check this option to send the user to a custom location, specified by the textbox above.', 'theme-my-login'); ?></p>
         </td>
     </tr>
 </table>
