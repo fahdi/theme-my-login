28de7c9bc98a93720a42373ede2188c62da9f0d4
jfarthing84 <jfarthing84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-07 03:58:47 +0000

    Remove dashes before instance in form field ID's.
    git-svn-id: https://plugins.svn.wordpress.org/theme-my-login/trunk@249625 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/register-form.php
+++ templates/register-form.php
@@ -1,18 +1,18 @@
-<div class="login" id="theme-my-login-<?php $template->the_current_instance(); ?>">
+<div class="login" id="theme-my-login<?php $template->the_current_instance(); ?>">
 	<?php $template->the_action_message( 'register' ); ?>
 	<?php $template->the_errors(); ?>
-    <form name="registerform" id="registerform-<?php $template->the_current_instance(); ?>" action="<?php $template->the_action_url( 'register' ); ?>" method="post">
+    <form name="registerform" id="registerform<?php $template->the_current_instance(); ?>" action="<?php $template->the_action_url( 'register' ); ?>" method="post">
         <p>
-            <label for="user_login-<?php $template->the_current_instance(); ?>"><?php _e( 'Username', 'theme-my-login' ) ?></label>
-            <input type="text" name="user_login" id="user_login-<?php $template->the_current_instance(); ?>" class="input" value="<?php $template->the_posted_value( 'user_login' ); ?>" size="20" />
+            <label for="user_login<?php $template->the_current_instance(); ?>"><?php _e( 'Username', 'theme-my-login' ) ?></label>
+            <input type="text" name="user_login" id="user_login<?php $template->the_current_instance(); ?>" class="input" value="<?php $template->the_posted_value( 'user_login' ); ?>" size="20" />
         </p>
         <p>
-            <label for="user_email-<?php $template->the_current_instance(); ?>"><?php _e( 'E-mail', 'theme-my-login' ) ?></label>
-            <input type="text" name="user_email" id="user_email-<?php $template->the_current_instance(); ?>" class="input" value="<?php $template->the_posted_value( 'user_email' ); ?>" size="20" />
+            <label for="user_email<?php $template->the_current_instance(); ?>"><?php _e( 'E-mail', 'theme-my-login' ) ?></label>
+            <input type="text" name="user_email" id="user_email<?php $template->the_current_instance(); ?>" class="input" value="<?php $template->the_posted_value( 'user_email' ); ?>" size="20" />
         </p>
 <?php do_action_ref_array( 'register_form', array( &$template ) ); ?>
         <p class="submit">
-            <input type="submit" name="wp-submit" id="wp-submit-<?php $template->the_current_instance(); ?>" value="<?php _e( 'Register', 'theme-my-login' ); ?>" />
+            <input type="submit" name="wp-submit" id="wp-submit<?php $template->the_current_instance(); ?>" value="<?php _e( 'Register', 'theme-my-login' ); ?>" />
         </p>
     </form>
 	<?php $template->the_action_links( array( 'register' => false ) ); ?>
