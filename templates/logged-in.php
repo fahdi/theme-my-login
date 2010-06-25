a0725f8cf3b741a4719930c4643c11d0a5cd5aa4
jfarthing84 <jfarthing84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-25 17:12:16 +0000

    Cleanup missed deletion.
    git-svn-id: https://plugins.svn.wordpress.org/theme-my-login/trunk@257289 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/logged-in.php
+++
@@ -1,7 +0,0 @@
-<div class="login" id="theme-my-login<?php $template->the_instance(); ?>">
-	<?php if ( $template->options['show_gravatar'] ) : ?>
-	<div class="tml-user-avatar"><?php $template->the_user_avatar(); ?></div>
-	<?php endif; ?>
-
-	<?php $this->the_user_links(); ?>
-</div>
