f7e5623be8a6dd83343e02b846262f3ac4c846b4
jfarthing84 <jfarthing84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-12 23:59:26 +0000

    Add "DYK" messages.
    git-svn-id: https://plugins.svn.wordpress.org/theme-my-login/trunk@263059 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/class-theme-my-login-admin.php
+++ admin/class-theme-my-login-admin.php
@@ -121,8 +121,9 @@ class Theme_My_Login_Admin {
 	 */
 	function did_you_know() {
 		$tips = apply_filters( 'tml_tips', array(
+			__( 'You can now login with your e-mail address or username! Try it out!', $this->theme_my_login->textdomain ),
 			__( 'Theme My Login now utilizes a module system. Modules are similar to WordPress plugins. Each module extends the default functionality of Theme My Login. <a rel="tml-options" href="#tml-modules">Click here</a> to get started with modules now.', $this->theme_my_login->textdomain ),
-			__( 'Theme My Login now allows custom forms. You can create your own form template(s) by copying the default version(s) from "theme-my-login/templates". Try it out!', $this->theme_my_login->textdomain ),
+			__( 'Theme My Login now allows custom forms. You can create your own form template(s) by copying the default version(s) from "theme-my-login/templates" to your current theme directory. Try it out!', $this->theme_my_login->textdomain ),
 			__( 'You can maintain your stylesheet changes between upgrades. Just simply copy the file "theme-my-login/theme-my-login.css" to your current theme directory and edit it as you please!', $this->theme_my_login->textdomain ),
 			__( 'Theme My Login provides a shortcode that you can use within your posts with multiple parameters to customize the form. Visit the <a href="http://www.jfarthing.com/docs/theme-my-login/shortcode" target="_blank">Theme My Login Documentation</a> for usage instructions.', $this->theme_my_login->textdomain ),
 			__( 'Theme My Login is <em>FREE</em> but Jeff sure appreciates <a href="http://www.jfarthing.com/donate" target="_blank">donations</a>!', $this->theme_my_login->textdomain ),
