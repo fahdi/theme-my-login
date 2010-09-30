7bb7f5c9b7383e37a5cd7736c0ba0e15eead6cc0
jfarthing84 <jfarthing84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-09-30 15:58:29 +0000

    Bump.
    git-svn-id: https://plugins.svn.wordpress.org/theme-my-login/trunk@295275 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- theme-my-login.php
+++ theme-my-login.php
@@ -3,7 +3,7 @@
 Plugin Name: Theme My Login
 Plugin URI: http://www.jfarthing.com/wordpress-plugins/theme-my-login/
 Description: Themes the WordPress login, registration and forgot password pages according to your theme.
-Version: 6.0
+Version: 6.1-alpha
 Author: Jeff Farthing
 Author URI: http://www.jfarthing.com
 Text Domain: theme-my-login
