dcd3cab1719c36da6efdf99a827fd4e6fc3e1867
jfarthing84 <jfarthing84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-26 04:40:20 +0000

    Generic multisite template tweaks.
    git-svn-id: https://plugins.svn.wordpress.org/theme-my-login/trunk@315684 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/ms-signup-user-form.php
+++ templates/ms-signup-user-form.php
@@ -4,49 +4,47 @@ If you would like to edit this file, copy it to your current theme's directory a
 Theme My Login will always look in your theme's directory first, before using this default template.
 */
 ?>
-<div class="login mu_register" id="theme-my-login<?php $template->the_instance(); ?>">
-	<h2><?php printf( __( 'Get your own %s account in seconds', $theme_my_login->textdomain ), $current_site->site_name ) ?></h2>
+<h2><?php printf( __( 'Get your own %s account in seconds', $theme_my_login->textdomain ), $current_site->site_name ); ?></h2>
 
-	<form id="setupform" method="post" action="">
-		<input type="hidden" name="stage" value="validate-user-signup" />
-		<?php do_action( 'signup_hidden_fields' ); ?>
+<form id="setupform" method="post" action="">
+	<input type="hidden" name="stage" value="validate-user-signup" />
+	<?php do_action( 'signup_hidden_fields' ); ?>
 
-		<label for="user_name<?php $template->the_instance(); ?>"><?php _e( 'Username:', $theme_my_login->textdomain ); ?></label>
-		<?php if ( $errmsg = $errors->get_error_message( 'user_name' ) ) { ?>
-			<p class="error"><?php echo $errmsg; ?></p>
-		<?php } ?>
+	<label for="user_name<?php $template->the_instance(); ?>"><?php _e( 'Username:', $theme_my_login->textdomain ); ?></label>
+	<?php if ( $errmsg = $errors->get_error_message( 'user_name' ) ) { ?>
+		<p class="error"><?php echo $errmsg; ?></p>
+	<?php } ?>
 
-		<input name="user_name" type="text" id="user_name<?php $template->the_instance(); ?>" value="<?php echo esc_attr( $user_name ); ?>" maxlength="60" /><br />
-		<span class="hint"><?php _e( '(Must be at least 4 characters, letters and numbers only.)', $theme_my_login->textdomain ); ?></span>
+	<input name="user_name" type="text" id="user_name<?php $template->the_instance(); ?>" value="<?php echo esc_attr( $user_name ); ?>" maxlength="60" /><br />
+	<span class="hint"><?php _e( '(Must be at least 4 characters, letters and numbers only.)', $theme_my_login->textdomain ); ?></span>
 
-		<label for="user_email<?php $template->the_instance(); ?>"><?php _e( 'Email&nbsp;Address:', $theme_my_login->textdomain ) ?></label>
-		<?php if ( $errmsg = $errors->get_error_message( 'user_email' ) ) { ?>
-			<p class="error"><?php echo $errmsg ?></p>
-		<?php } ?>
-		
-		<input name="user_email" type="text" id="user_email<?php $template->the_instance(); ?>" value="<?php echo esc_attr( $user_email ) ?>" maxlength="200" /><br />
-		<span class="hint"><?php _e( 'We send your registration email to this address. (Double-check your email address before continuing.)', $theme_my_login->textdomain ) ?></span>
-		<?php if ( $errmsg = $errors->get_error_message( 'generic' ) ) { ?>
-			<p class="error"><?php echo $errmsg; ?></p>
-		<?php } ?>
+	<label for="user_email<?php $template->the_instance(); ?>"><?php _e( 'Email&nbsp;Address:', $theme_my_login->textdomain ); ?></label>
+	<?php if ( $errmsg = $errors->get_error_message( 'user_email' ) ) { ?>
+		<p class="error"><?php echo $errmsg ?></p>
+	<?php } ?>
+	
+	<input name="user_email" type="text" id="user_email<?php $template->the_instance(); ?>" value="<?php echo esc_attr( $user_email ); ?>" maxlength="200" /><br />
+	<span class="hint"><?php _e( 'We send your registration email to this address. (Double-check your email address before continuing.)', $theme_my_login->textdomain ); ?></span>
+	<?php if ( $errmsg = $errors->get_error_message( 'generic' ) ) { ?>
+		<p class="error"><?php echo $errmsg; ?></p>
+	<?php } ?>
 
-		<?php do_action( 'signup_extra_fields', $errors ); ?>
+	<?php do_action( 'signup_extra_fields', $errors ); ?>
 
-		<p>
-		<?php if ( $active_signup == 'blog' ) { ?>
-			<input id="signupblog<?php $template->the_instance(); ?>" type="hidden" name="signup_for" value="blog" />
-		<?php } elseif ( $active_signup == 'user' ) { ?>
-			<input id="signupblog<?php $template->the_instance(); ?>" type="hidden" name="signup_for" value="user" />
-		<?php } else { ?>
-			<input id="signupblog<?php $template->the_instance(); ?>" type="radio" name="signup_for" value="blog" <?php if ( !isset( $_POST['signup_for'] ) || $_POST['signup_for'] == 'blog' ) { ?>checked="checked"<?php } ?> />
-			<label class="checkbox" for="signupblog"><?php _e( 'Gimme a site!', $theme_my_login->textdomain ) ?></label>
-			<br />
-			<input id="signupuser<?php $template->the_instance(); ?>" type="radio" name="signup_for" value="user" <?php if ( isset( $_POST['signup_for'] ) && $_POST['signup_for'] == 'user' ) { ?>checked="checked"<?php } ?> />
-			<label class="checkbox" for="signupuser"><?php _e( 'Just a username, please.', $theme_my_login->textdomain ) ?></label>
-		<?php } ?>
-		</p>
+	<p>
+	<?php if ( $active_signup == 'blog' ) { ?>
+		<input id="signupblog<?php $template->the_instance(); ?>" type="hidden" name="signup_for" value="blog" />
+	<?php } elseif ( $active_signup == 'user' ) { ?>
+		<input id="signupblog<?php $template->the_instance(); ?>" type="hidden" name="signup_for" value="user" />
+	<?php } else { ?>
+		<input id="signupblog<?php $template->the_instance(); ?>" type="radio" name="signup_for" value="blog" <?php if ( !isset( $_POST['signup_for'] ) || $_POST['signup_for'] == 'blog' ) { ?>checked="checked"<?php } ?> />
+		<label class="checkbox" for="signupblog"><?php _e( 'Gimme a site!', $theme_my_login->textdomain ); ?></label>
+		<br />
+		<input id="signupuser<?php $template->the_instance(); ?>" type="radio" name="signup_for" value="user" <?php if ( isset( $_POST['signup_for'] ) && $_POST['signup_for'] == 'user' ) { ?>checked="checked"<?php } ?> />
+		<label class="checkbox" for="signupuser"><?php _e( 'Just a username, please.', $theme_my_login->textdomain ); ?></label>
+	<?php } ?>
+	</p>
 
-		<p class="submit"><input type="submit" name="submit" class="submit" value="<?php esc_attr_e( 'Next', $theme_my_login->textdomain ) ?>" /></p>
-	</form>
-	<?php $template->the_action_links( array( 'register' => false ) ); ?>
-</div>
+	<p class="submit"><input type="submit" name="submit" class="submit" value="<?php esc_attr_e( 'Next', $theme_my_login->textdomain ); ?>" /></p>
+</form>
+<?php $template->the_action_links( array( 'register' => false ) ); ?>
