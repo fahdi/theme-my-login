a4243813bfa4d037578df4cade65a75f8f37a48f
jfarthing84 <jfarthing84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-03 14:55:38 +0000

    Replace get_userdatabylogin() with get_user_by( 'login' ).
    git-svn-id: https://plugins.svn.wordpress.org/theme-my-login/trunk@552500 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-theme-my-login.php
+++ includes/class-theme-my-login.php
@@ -275,7 +275,7 @@ class Theme_My_Login extends Theme_My_Login_Abstract {
 					// If the user wants ssl but the session is not ssl, force a secure cookie.
 					if ( !empty( $_POST['log'] ) && !force_ssl_admin() ) {
 						$user_name = sanitize_user( $_POST['log'] );
-						if ( $user = get_userdatabylogin( $user_name ) ) {
+						if ( $user = get_user_by( 'login', $user_name ) ) {
 							if ( get_user_option( 'use_ssl', $user->ID ) ) {
 								$secure_cookie = true;
 								force_ssl_admin( true );
