9770b51768ba2a12981a5f5a75ccda708fe1dc9d
jfarthing84 <jfarthing84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-26 19:13:11 +0000

    Remove "tml_lostpassword_form" action.
    git-svn-id: https://plugins.svn.wordpress.org/theme-my-login/trunk@564194 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- templates/lostpassword-form.php
+++ templates/lostpassword-form.php
@@ -12,10 +12,9 @@ Theme My Login will always look in your theme's directory first, before using th
 			<label for="user_login<?php $template->the_instance(); ?>"><?php _e( 'Username or E-mail:' ); ?></label>
 			<input type="text" name="user_login" id="user_login<?php $template->the_instance(); ?>" class="input" value="<?php $template->the_posted_value( 'user_login' ); ?>" size="20" tabindex="10" />
 		</p>
-<?php
-do_action( 'lostpassword_form' ); // Wordpress hook
-do_action_ref_array( 'tml_lostpassword_form', array( &$template ) ); // TML hook
-?>
+
+		<?php do_action( 'lostpassword_form' ); ?>
+
 		<p class="submit">
 			<input type="submit" name="wp-submit" id="wp-submit<?php $template->the_instance(); ?>" value="<?php esc_attr_e( 'Get New Password' ); ?>" tabindex="100" />
 			<input type="hidden" name="redirect_to" value="<?php $template->the_redirect_url( 'lostpassword' ); ?>" />
