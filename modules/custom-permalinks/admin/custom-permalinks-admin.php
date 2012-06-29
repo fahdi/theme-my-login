8d506979c22d4b1bc74855e073ff92d421c918dd
jfarthing84 <jfarthing84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-29 15:03:54 +0000

    Add visual cue to show TML permalink structure.
    git-svn-id: https://plugins.svn.wordpress.org/theme-my-login/trunk@565460 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/custom-permalinks/admin/custom-permalinks-admin.php
+++ modules/custom-permalinks/admin/custom-permalinks-admin.php
@@ -144,9 +144,11 @@ class Theme_My_Login_Custom_Permalinks_Admin extends Theme_My_Login_Abstract {
 	 * @access public
 	 */
 	public function settings_field_permalink( $args = '' ) {
+		global $theme_my_login;
 		extract( $args );
 		?>
 		<input name="<?php echo $this->options_key; ?>[<?php echo $action; ?>]" type="text" id="<?php echo $this->options_key; ?>_<?php echo $action; ?>" value="<?php echo $this->get_option( $action ); ?>" class="regular-text" />
+		<p class="description"><strong><?php _e( 'Permalink:' ); ?></strong> <span id="sample-permalink"><?php echo $theme_my_login->get_login_page_link( compact( 'action' ) ); ?></span></p>
 		<?php
 	}
 
