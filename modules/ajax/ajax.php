590768097b97bcf432464ddf410008edde7da7eb
jfarthing84 <jfarthing84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-24 18:07:45 +0000

    Make AJAX and moderation play well together.
    git-svn-id: https://plugins.svn.wordpress.org/theme-my-login/trunk@616961 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/ajax/ajax.php
+++ modules/ajax/ajax.php
@@ -40,11 +40,12 @@ class Theme_My_Login_Ajax extends Theme_My_Login_Abstract {
 		add_action( 'template_redirect',  array( &$this, 'template_redirect'  ) );
 		add_action( 'wp_enqueue_scripts', array( &$this, 'wp_enqueue_scripts' ) );
 
-		add_filter( 'tml_page_link',          array( &$this, 'tml_page_link'          ), 10, 3 );
-		add_filter( 'tml_action_url',         array( &$this, 'tml_action_url'         ), 10, 3 );
-		add_filter( 'tml_redirect_url',       array( &$this, 'tml_redirect_url'       ), 10, 2 );
-		add_filter( 'page_css_class',         array( &$this, 'page_css_class'         ), 10, 2 );
-		add_filter( 'wp_setup_nav_menu_item', array( &$this, 'wp_setup_nav_menu_item' )        );
+		add_filter( 'tml_page_link',          array( &$this, 'tml_page_link'          ),  10, 3 );
+		add_filter( 'tml_action_url',         array( &$this, 'tml_action_url'         ),  10, 3 );
+		add_filter( 'tml_redirect_url',       array( &$this, 'tml_redirect_url'       ),  10, 2 );
+		add_filter( 'register_redirect',      array( &$this, 'register_redirect'      ), 200    );
+		add_filter( 'page_css_class',         array( &$this, 'page_css_class'         ),  10, 2 );
+		add_filter( 'wp_setup_nav_menu_item', array( &$this, 'wp_setup_nav_menu_item' )         );
 	}
 
 	/**
@@ -56,7 +57,10 @@ class Theme_My_Login_Ajax extends Theme_My_Login_Abstract {
 	 * @return array AJAX actions
 	 */
 	public static function default_actions() {
-		return apply_filters( 'tml_ajax_actions', array( 'login', 'register', 'lostpassword' ) );
+		$actions = array( 'login', 'register', 'lostpassword' );
+		if ( is_multisite() )
+			$actions[] = 'activate';
+		return apply_filters( 'tml_ajax_actions', $actions );
 	}
 
 	public function template_redirect() {
@@ -137,6 +141,15 @@ class Theme_My_Login_Ajax extends Theme_My_Login_Abstract {
 				$url = add_query_arg( 'instance', $_GET['instance'], $url );
 		}
 		return $url;
+	}
+
+	public function register_redirect( $redirect_to ) {
+		$theme_my_login = Theme_My_Login::get_object();
+
+		$action = $theme_my_login->request_action ? $theme_my_login->request_action : 'login';
+		if ( in_array( $action, self::default_actions() ) && isset( $_GET['ajax'] ) )
+			$redirect_to = add_query_arg( 'ajax', 1, $redirect_to );
+		return $redirect_to;
 	}
 
 	public function page_css_class( $classes, $page ) {
