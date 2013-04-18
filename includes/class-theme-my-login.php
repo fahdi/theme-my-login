964ab45b4f6aa66ecd9f22dfc2447c2103288804
jfarthing84 <jfarthing84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-18 14:36:08 +0000

    6.3 branch is now 6.3.6. Unashamed.
    git-svn-id: https://plugins.svn.wordpress.org/theme-my-login/branches/6.3@699795 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-theme-my-login.php
+++ includes/class-theme-my-login.php
@@ -21,7 +21,7 @@ class Theme_My_Login extends Theme_My_Login_Abstract {
 	 * @since 6.3.2
 	 * @const string
 	 */
-	const version = '6.3.5';
+	const version = '6.3.6';
 
 	/**
 	 * Holds options key
