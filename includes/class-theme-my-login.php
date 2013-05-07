e8949711e51094ecfbfe1ffa220257b5737f90bb
jfarthing84 <jfarthing84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-07 18:21:25 +0000

    Use null logic instead of boolean in get_page_id.
    git-svn-id: https://plugins.svn.wordpress.org/theme-my-login/trunk@709399 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-theme-my-login.php
+++ includes/class-theme-my-login.php
@@ -965,9 +965,10 @@ if(typeof wpOnload=='function')wpOnload()
 		elseif ( 'retrievepassword' == $action )
 			$action = 'lostpassword';
 
-		$page_id = wp_cache_get( $action, 'tml_page_ids' );
-		if ( false === $page_id ) {
+		if ( ! $page_id = wp_cache_get( $action, 'tml_page_ids' ) ) {
 			$page_id = $wpdb->get_var( $wpdb->prepare( "SELECT p.ID FROM $wpdb->posts p LEFT JOIN $wpdb->postmeta pmeta ON p.ID = pmeta.post_id WHERE p.post_type = 'page' AND pmeta.meta_key = '_tml_action' AND pmeta.meta_value = %s", $action ) );
+			if ( ! $page_id )
+				return null;
 			wp_cache_add( $action, $page_id, 'tml_page_ids' );
 		}
 		return $page_id;
